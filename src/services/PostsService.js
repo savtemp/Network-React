d7ebc16db6c0cde99685f4b2806a06e47df09007
savtemp <savtemple@gmail.com>
2023-03-13 11:39:21 -0600

    edited get all posts

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -1,21 +1,38 @@
 import { AppState } from "../AppState.js";
-import { Post } from "../models/Post.js";
 import { api } from "./AxiosService.js"
 
 
 class PostsService{
-  getProfilePosts(id) {
-    throw new Error('Method not implemented.');
+
+  async getActiveProfile(id){
+    AppState.activeProfile = null
+    const res = await api.get('api/profiles/' + id)
+    AppState.activeProfile = res.data
   }
 
-  async getAllPosts() {
-    AppState.posts = []
-    const res = await api.get('api/posts')
-    console.log('[GETTING CARS]', res.data);
-    AppState.posts = res.data.posts.map(post => new Post(post))
-    console.log(AppState.posts);
+  async getProfilePosts(id) {
+    // eslint-disable-next-line no-undef
+    await Promise.allSettled([
+      this.getActiveProfile(id),
+      this.getAllPosts('api/posts?creatorId =' + id)
+    ])
   }
 
+  async getAllPosts(url = 'api/posts') {
+    // AppState.posts = []
+    // const res = await api.get('api/posts')
+    // console.log('[GETTING CARS]', res.data);
+    // AppState.posts = res.data.posts.map(post => new Post(post))
+    // console.log(AppState.posts);
+
+    const res = await api.get('url')
+    const posts = res.data.posts
+    const newer = res.data.newer
+    const older = res.data.older
+    AppState.posts = posts
+    AppState.newerPage = newer
+    AppState.olderPage = older
+  }
 }
 
 
