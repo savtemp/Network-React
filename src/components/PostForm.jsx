a10fece9647bc68ff62db4612ab1d64e40357ee7
savtemp <savtemple@gmail.com>
2023-03-20 12:19:36 -0600

    created post form component

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,14 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+function PostForm() {
+
+  return (
+
+    <div className="PostForm">
+
+    </div>
+  )
+
+}
+export default observer(PostForm)
